a49b67238a54e19b4d63d7993d51db4c1c93b662
Chaitanya Narayana Murthy Anisetti <chaitanya.anisetti@gmail.com>
2025-11-13 06:55:48 +0000

    first commit

---
+++ sonarqube_demo/webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("sqdemo.sonarqubedemo.controller.View1", {
+        onInit() {
+        }
+    });
+});
